41b156280eee3b3936ac9a4485261620872e93e9
Chris Statzer <chris.statzer@gmail.com>
2016-02-08 20:44:43 +0800

    Added a keyboard event and an example of how to expose an enum.

--- src/SDL2Application.cpp
+++ src/SDL2Application.cpp
@@ -8,24 +8,41 @@
 #include "SDL_image.h"
 
 
+enum KEYS {
+  KEY_ESC = SDL_SCANCODE_ESCAPE,
+  KEY_UP = SDL_SCANCODE_UP,
+  KEY_DOWN = SDL_SCANCODE_DOWN,
+  KEY_LEFT = SDL_SCANCODE_LEFT,
+  KEY_RIGHT = SDL_SCANCODE_RIGHT
+ };
+
+
+class KeyboardEvent {
+public:
+  int key = 0;
+};
+
+
 class SDL2Application {
   SDL_Window *window;
   SDL_Renderer *renderer;
   SDL_Texture *sheet;
   bool running;
+  boost::python::list keyboardEvents;
+
 public:
   void init(int width, int height) {
-    running = true;
     std::cout << "SDL2Application::init" << std::endl;
-  	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
   		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
   	}
 
     window = SDL_CreateWindow("Foo", 100, 100, width, height, SDL_WINDOW_SHOWN);
-    if (window == nullptr){
+    if (window == nullptr) {
     	std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
     	SDL_Quit();
     }
+
     renderer = SDL_CreateRenderer(window, -1,
       SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) {
@@ -33,7 +50,9 @@ public:
     	std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
     	SDL_Quit();
     }
+
     sheet = loadTexture("../assets/alpha-sheet.png");
+    running = true;
   }
 
   void clear(void) {
@@ -45,6 +64,7 @@ public:
   }
 
   void quit(void) {
+    running = false;
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -74,24 +94,49 @@ public:
   void update(void) {
     if (!running)
       quit();
+
     SDL_Event e;
 		while (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT) {
-				std::cout << "SDL_QUIT" << std::endl;
-				running = false;
-			}
+			if (e.type == SDL_QUIT)
+				quit();
+      if (e.type == SDL_KEYDOWN)
+        onKeydown(e.key.keysym.scancode);
 		}
+  }
+
+  boost::python::list pollKeys(void) {
+    boost::python::list listToReturn = keyboardEvents;
+    keyboardEvents = boost::python::list();
+    return listToReturn;
   }
 
   bool isRunning(void) {
     return running;
   }
+
+  void onKeydown(int k) {
+    KeyboardEvent key;
+    key.key = k;
+    keyboardEvents.append(key);
+  }
 };
+
 
 
 BOOST_PYTHON_MODULE(sdlapp)
 {
     using namespace boost::python;
+    enum_<KEYS>("keys")
+    .value("KEY_ESC", KEY_ESC)
+    .value("KEY_UP", KEY_UP)
+    .value("KEY_DOWN", KEY_DOWN)
+    .value("KEY_LEFT", KEY_LEFT)
+    .value("KEY_RIGHT", KEY_RIGHT)
+        ;
+
+    class_<KeyboardEvent>("KeyboardEvent")
+        .add_property("key", &KeyboardEvent::key);
+
     class_<SDL2Application>("SDL2Application")
         .def("init", &SDL2Application::init)
         .def("draw_char", &SDL2Application::drawChar)
@@ -100,5 +145,6 @@ BOOST_PYTHON_MODULE(sdlapp)
         .def("update", &SDL2Application::update)
         .def("quit", &SDL2Application::quit)
         .def("is_running", &SDL2Application::isRunning)
+        .def("poll_keys", &SDL2Application::pollKeys)
     ;
 }
--- test_sdlapp.py
+++ test_sdlapp.py
@@ -3,13 +3,20 @@ from random import randint
 from sdlapp import SDL2Application
 
 
-app = SDL2Application()
+class MyApp(SDL2Application):
+    def draw(self):
+        for y in range(0, 45):
+            for x in range(0, 80):
+                self.draw_char(x, y, randint(0, 254))
+
+    def on_keypress(self, key):
+        print 'MyApp::on_keypress({})'.format(key)
+
+app = MyApp()
 app.init(1280, 720)
 
 while app.is_running() is True:
     app.update()
     app.clear()
-    for y in range(0, 40):
-        for x in range(0, 70):
-            app.draw_char(x, y, randint(0, 254))
+    app.draw()
     app.flip()
