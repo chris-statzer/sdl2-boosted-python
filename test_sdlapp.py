ba77074b15042ae567d4bb95c9d380254311cdb8
Chris Statzer <chris.statzer@gmail.com>
2016-02-08 20:51:56 +0800

    made a more practical example script

--- test_sdlapp.py
+++ test_sdlapp.py
@@ -1,22 +1,35 @@
 from random import randint
 
-from sdlapp import SDL2Application
+from sdlapp import SDL2Application, KeyboardEvent, keys
 
 
 class MyApp(SDL2Application):
-    def draw(self):
-        for y in range(0, 45):
-            for x in range(0, 80):
-                self.draw_char(x, y, randint(0, 254))
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.player_x = 10
+        self.player_y = 10
 
-    def on_keypress(self, key):
-        print 'MyApp::on_keypress({})'.format(key)
+    def draw(self):
+        self.draw_char(self.player_x, self.player_y, 1)
+
 
 app = MyApp()
 app.init(1280, 720)
 
+
 while app.is_running() is True:
     app.update()
+    for e in app.poll_keys():
+        if e.key == keys.KEY_ESC:
+            app.quit()
+        elif e.key == keys.KEY_UP:
+            app.player_y -= 1
+        elif e.key == keys.KEY_DOWN:
+            app.player_y += 1
+        elif e.key == keys.KEY_LEFT:
+            app.player_x -= 1
+        elif e.key == keys.KEY_RIGHT:
+            app.player_x += 1
     app.clear()
     app.draw()
     app.flip()
